6c39afbef7afc7f2cdd97ae3ebc2f3a1e73abc3c
amitshankar97 <amitarunshankar@gmail.com>
2018-10-29 23:25:41 -0700

    a

--- properties/index.js
+++ properties/index.js
@@ -2,6 +2,22 @@ const MILE_TO_METER = 1609.34;
 var maps = require('../gMaps');
 var db = require('../db');
 
+module.exports = async function (context, req) {
+
+    if (!req.query.lat || !req.query.lon) {
+        return {
+            status: 400,
+            body: 'Please provide valid coordinates'
+        };
+    }
+
+    let radius = req.query.radius ? (Number(req.query.radius) * MILE_TO_METER) : null;
+
+    return maps.getProperties(req.query.lat, req.query.lon, radius)
+        .then((properties) => appendDBProperties(properties))
+        .catch(err => context.log(err));
+};
+
 async function appendDBProperties(googleProperties) {
     let res = [];
 
@@ -28,19 +44,3 @@ async function appendDBProperties(googleProperties) {
 
     return res;
 }
-
-module.exports = async function (context, req) {
-
-    if (!req.query.lat || !req.query.lon) {
-        return {
-            status: 400,
-            body: 'Please provide valid coordinates'
-        };
-    }
-
-    let radius = req.query.radius ? (Number(req.query.radius) * MILE_TO_METER) : null;
-
-    return maps.getProperties(req.query.lat, req.query.lon, radius)
-        .then((properties) => appendDBProperties(properties))
-        .catch(err => context.log(err));
-};
